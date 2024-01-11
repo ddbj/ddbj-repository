5f4867a7e0c3537204469768167d561380732208
Keita Urashima <ursm@ursm.jp>
2024-01-11 19:10:36 +0900

    Fetch login URL from API

--- web/app/routes/login.ts
+++ web/app/routes/login.ts
@@ -1,6 +1,8 @@
 import Route from '@ember/routing/route';
 import { service } from '@ember/service';
 
+import ENV from 'ddbj-repository/config/environment';
+
 import type CurrentUserService from 'ddbj-repository/services/current-user';
 
 export default class LoginRoute extends Route {
@@ -8,5 +10,11 @@ export default class LoginRoute extends Route {
 
   beforeModel() {
     this.currentUser.ensureLogout();
+  }
+
+  async model() {
+    const res = await fetch(`${ENV.apiURL}/api-key`);
+
+    return await res.json();
   }
 }
