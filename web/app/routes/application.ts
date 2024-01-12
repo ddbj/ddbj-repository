631cea7601b0f58f2ad621d8772b83e6a1cc7098
Keita Urashima <ursm@ursm.jp>
2024-01-12 10:01:51 +0900

    Handle login failures properly

--- web/app/routes/application.ts
+++ web/app/routes/application.ts
@@ -4,12 +4,22 @@ import { service } from '@ember/service';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.css';
 
+import { LoginError } from 'ddbj-repository/services/current-user';
+
 import type CurrentUserService from 'ddbj-repository/services/current-user';
+import type Router from '@ember/routing/router';
 
 export default class ApplicationRoute extends Route {
   @service declare currentUser: CurrentUserService;
+  @service declare router: Router;
 
   async beforeModel() {
-    await this.currentUser.restore();
+    try {
+      await this.currentUser.restore();
+    } catch (err) {
+      if (!(err instanceof LoginError)) throw err;
+
+      this.router.transitionTo('login');
+    }
   }
 }
