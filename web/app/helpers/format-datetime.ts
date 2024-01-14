07d8ff1078ee4fb08f66c3ae901a0ad7d8e7f6e8
Keita Urashima <ursm@ursm.jp>
2024-01-14 11:05:06 +0900

    Use ISO8601-like datetime format

--- web/app/helpers/format-datetime.ts
+++ web/app/helpers/format-datetime.ts
@@ -5,5 +5,16 @@ export default function formatDatetime(date?: Date | string) {
     date = new Date(date);
   }
 
-  return `${date.toLocaleDateString('ja-JP')} ${date.toLocaleTimeString('ja-JP')}`;
+  const year = padZero(date.getFullYear());
+  const month = padZero(date.getMonth() + 1);
+  const _date = padZero(date.getDate());
+  const hours = padZero(date.getHours());
+  const minutes = padZero(date.getMinutes());
+  const seconds = padZero(date.getSeconds());
+
+  return `${year}-${month}-${_date} ${hours}:${minutes}:${seconds}`;
+}
+
+function padZero(n: number) {
+  return n.toString().padStart(2, '0');
 }
---
+++ web/tests/integration/helpers/format-datetime-test.ts
@@ -0,0 +1,30 @@
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'ddbj-repository/tests/helpers';
+import { render } from '@ember/test-helpers';
+import { hbs } from 'ember-cli-htmlbars';
+
+module('Integration | Helper | format-datetime', function (hooks) {
+  setupRenderingTest(hooks);
+
+  test('date instance', async function (assert) {
+    this.set('date', new Date(2024, 0, 2, 3, 4, 56));
+
+    await render(hbs`{{format-datetime this.date}}`);
+
+    assert.dom().hasText('2024-01-02 03:04:56');
+  });
+
+  test('string', async function (assert) {
+    this.set('date', new Date(2024, 0, 2, 3, 4, 56).toISOString());
+
+    await render(hbs`{{format-datetime this.date}}`);
+
+    assert.dom().hasText('2024-01-02 03:04:56');
+  });
+
+  test('undefined', async function (assert) {
+    await render(hbs`{{format-datetime undefined}}`);
+
+    assert.dom().hasNoText();
+  });
+});
