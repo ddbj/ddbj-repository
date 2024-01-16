e5e37603c3344d89db7abc10af0a3baaf337f5e5
Keita Urashima <ursm@ursm.jp>
2024-01-16 18:13:21 +0900

    Check for duplicate submissions more reliably

--- api/config/initializers/rambulance.rb
+++ api/config/initializers/rambulance.rb
@@ -2,7 +2,6 @@ require 'rambulance/exceptions_app'
 
 Rambulance.setup do |config|
   config.rescue_responses = {
-    'SubmissionsController::UnprocessableEntity'  => :unprocessable_entity,
     'Validations::FilesController::NotFound'      => :not_found,
     'Validations::ViaFilesController::BadRequest' => :bad_request
   }
