b7179425e336b54f5a3036a823785ca47185c1be
Keita Urashima <ursm@ursm.jp>
2024-01-19 15:50:34 +0900

    Create/list/show submission

--- web/app/controllers/validations/show.ts
+++ web/app/controllers/validations/show.ts
@@ -1,22 +1,91 @@
 import Controller from '@ember/controller';
 import { action } from '@ember/object';
 import { service } from '@ember/service';
+import { tracked } from '@glimmer/tracking';
+
+import { task } from 'ember-concurrency';
+
+import ENV from 'ddbj-repository/config/environment';
+import downloadFile from 'ddbj-repository/utils/download-file';
 
 import type CurrentUserService from 'ddbj-repository/services/current-user';
+import type ErrorModalService from 'ddbj-repository/services/error-modal';
+import type Router from '@ember/routing/router';
+
+const oneDay = 24 * 60 * 60 * 1000;
+
+type Validation = {
+  id: number;
+  validity?: string;
+  finished_at?: string;
+
+  submission?: {
+    id: string;
+  };
+};
 
 export default class ValidationsShowController extends Controller {
   @service declare currentUser: CurrentUserService;
+  @service declare errorModal: ErrorModalService;
+  @service declare router: Router;
+
+  @tracked currentTime = new Date();
+
+  timer: number;
+
+  constructor(owner: object | undefined) {
+    super(owner);
+
+    this.timer = setInterval(() => {
+      this.currentTime = new Date();
+    }, 1000);
+  }
+
+  get canSubmit() {
+    return !this.cannotSubmitReason;
+  }
+
+  get cannotSubmitReason() {
+    const { validity, finished_at, submission } = this.model as Validation;
+
+    if (submission) {
+      return 'Validation is already submitted.';
+    } else if (validity !== 'valid') {
+      return 'Validation must be valid.';
+    } else if (this.currentTime.getTime() - new Date(finished_at!).getTime() >= oneDay) {
+      return 'Validation must finished in 24 hours.';
+    } else {
+      return undefined;
+    }
+  }
 
   @action
   async downloadFile(url: string) {
-    const res = await fetch(url, {
-      headers: this.currentUser.authorizationHeader,
+    await downloadFile(url, this.currentUser);
+  }
+
+  submit = task({ drop: true }, async () => {
+    const { id } = this.model as Validation;
+
+    const res = await fetch(`${ENV.apiURL}/submissions`, {
+      method: 'POST',
+
+      headers: {
+        'Content-Type': 'application/json',
+        ...this.currentUser.authorizationHeader,
+      },
+
+      body: JSON.stringify({
+        validation_id: id,
+      }),
     });
 
     if (!res.ok) {
-      throw new Error(res.statusText);
+      this.errorModal.show(new Error(res.statusText));
     }
 
-    location.href = res.url;
-  }
+    const { id: submissionId } = await res.json();
+
+    this.router.transitionTo('submissions.show', submissionId);
+  });
 }
