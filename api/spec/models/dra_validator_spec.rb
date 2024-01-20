6c275af9ec3f36b60adc2664efad136fb64423aa
Keita Urashima <ursm@ursm.jp>
2024-01-20 20:01:53 +0900

    Remove "Objects" section from validations.show

--- api/spec/models/dra_validator_spec.rb
+++ api/spec/models/dra_validator_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe DraValidator, type: :model do
   example 'valid' do
-    validation = create(:validation, db: 'DRA') {|validation|
+    validation = create(:validation, id: 42, db: 'DRA') {|validation|
       create :obj, validation:, _id: 'Submission', file: file_fixture_upload('dra/valid/example-0001_dra_Submission.xml')
       create :obj, validation:, _id: 'Experiment', file: file_fixture_upload('dra/valid/example-0001_dra_Experiment.xml')
       create :obj, validation:, _id: 'Run',        file: file_fixture_upload('dra/valid/example-0001_dra_Run.xml')
@@ -19,39 +19,55 @@ RSpec.describe DraValidator, type: :model do
     expect(validation.validation_reports).to contain_exactly(
       {
         object_id: '_base',
-        path:      nil,
         validity:  'valid',
-        details:   nil
+        details:   nil,
+        file:      nil
       },
       {
         object_id: 'Submission',
-        path:      'example-0001_dra_Submission.xml',
         validity:  'valid',
-        details:   nil
+        details:   nil,
+
+        file: {
+          path: 'example-0001_dra_Submission.xml',
+          url:  'http://www.example.com/api/validations/42/files/example-0001_dra_Submission.xml'
+        }
       },
       {
         object_id: 'Experiment',
-        path:      'example-0001_dra_Experiment.xml',
         validity:  'valid',
-        details:   nil
+        details:   nil,
+
+        file: {
+          path: 'example-0001_dra_Experiment.xml',
+          url:  'http://www.example.com/api/validations/42/files/example-0001_dra_Experiment.xml'
+        }
       },
       {
         object_id: 'Run',
-        path:      'example-0001_dra_Run.xml',
         validity:  'valid',
-        details:   nil
+        details:   nil,
+
+        file: {
+          path: 'example-0001_dra_Run.xml',
+          url:  'http://www.example.com/api/validations/42/files/example-0001_dra_Run.xml'
+        }
       },
       {
         object_id: 'RunFile',
-        path:      'runfile.xml',
         validity:  'valid',
-        details:   nil
+        details:   nil,
+
+        file: {
+          path: 'runfile.xml',
+          url:  'http://www.example.com/api/validations/42/files/runfile.xml'
+        }
       }
     )
   end
 
   example 'invalid' do
-    validation = create(:validation, db: 'DRA') {|validation|
+    validation = create(:validation, id: 42, db: 'DRA') {|validation|
       create :obj, validation:, _id: 'Submission', file: file_fixture_upload('dra/invalid/example-0001_dra_Submission.xml')
       create :obj, validation:, _id: 'Experiment', file: file_fixture_upload('dra/invalid/example-0001_dra_Experiment.xml')
       create :obj, validation:, _id: 'Run',        file: file_fixture_upload('dra/invalid/example-0001_dra_Run.xml')
@@ -68,37 +84,53 @@ RSpec.describe DraValidator, type: :model do
     expect(validation.validation_reports).to contain_exactly(
       {
         object_id: '_base',
-        path:      nil,
         validity:  'valid',
-        details:   nil
+        details:   nil,
+        file:      nil
       },
       {
         object_id: 'Submission',
-        path:      'example-0001_dra_Submission.xml',
         validity:  'invalid',
 
         details: [
           'object_id' => 'Submission',
           'message'   => '18:1: FATAL: Premature end of data in tag SUBMISSION line 2'
-        ]
+        ],
+
+        file: {
+          path: 'example-0001_dra_Submission.xml',
+          url:  'http://www.example.com/api/validations/42/files/example-0001_dra_Submission.xml'
+        }
       },
       {
         object_id: 'Experiment',
-        path:      'example-0001_dra_Experiment.xml',
         validity:  'invalid',
-        details:   instance_of(Array)
+        details:   instance_of(Array),
+
+        file: {
+          path: 'example-0001_dra_Experiment.xml',
+          url:  'http://www.example.com/api/validations/42/files/example-0001_dra_Experiment.xml'
+        }
       },
       {
         object_id: 'Run',
-        path:      'example-0001_dra_Run.xml',
         validity:  'invalid',
-        details:   instance_of(Array)
+        details:   instance_of(Array),
+
+        file: {
+          path: 'example-0001_dra_Run.xml',
+          url:  'http://www.example.com/api/validations/42/files/example-0001_dra_Run.xml'
+        }
       },
       {
         object_id: 'RunFile',
-        path:      'runfile.xml',
         validity:  'valid',
-        details:   nil
+        details:   nil,
+
+        file: {
+          path: 'runfile.xml',
+          url:  'http://www.example.com/api/validations/42/files/runfile.xml'
+        }
       }
     )
   end
@@ -122,19 +154,20 @@ RSpec.describe DraValidator, type: :model do
 
     expect(validation.validation_reports).to include(
       object_id: '_base',
-      path:      nil,
       validity:  'error',
 
       details: {
         'error' => 'Something went wrong.'
-      }
+      },
+
+      file: nil
     )
 
     expect(validation.submission).to be_nil
   end
 
   example 'with Analysis and AnalysisFile, valid' do
-    validation = create(:validation, db: 'DRA') {|validation|
+    validation = create(:validation, id: 42, db: 'DRA') {|validation|
       create :obj, validation:, _id: 'Submission',   file: file_fixture_upload('dra/valid/example-0002_dra_Submission.xml')
       create :obj, validation:, _id: 'Experiment',   file: file_fixture_upload('dra/valid/example-0002_dra_Experiment.xml')
       create :obj, validation:, _id: 'Run',          file: file_fixture_upload('dra/valid/example-0002_dra_Run.xml')
@@ -153,21 +186,29 @@ RSpec.describe DraValidator, type: :model do
     expect(validation.validation_reports).to include(
       {
         object_id: 'Analysis',
-        path:      'example-0002_dra_Analysis.xml',
         validity:  'valid',
-        details:   nil
+        details:   nil,
+
+        file: {
+          path: 'example-0002_dra_Analysis.xml',
+          url:  'http://www.example.com/api/validations/42/files/example-0002_dra_Analysis.xml'
+        }
       },
       {
         object_id: 'AnalysisFile',
-        path:      'analysisfile.xml',
         validity:  'valid',
-        details:   nil
+        details:   nil,
+
+        file: {
+          path: 'analysisfile.xml',
+          url:  'http://www.example.com/api/validations/42/files/analysisfile.xml'
+        }
       }
     )
   end
 
   example 'with multiple RunFile, valid' do
-    validation = create(:validation, db: 'DRA') {|validation|
+    validation = create(:validation, id: 42, db: 'DRA') {|validation|
       create :obj, validation:, _id: 'Submission', file: file_fixture_upload('dra/valid/example-0001_dra_Submission.xml')
       create :obj, validation:, _id: 'Experiment', file: file_fixture_upload('dra/valid/example-0001_dra_Experiment.xml')
       create :obj, validation:, _id: 'Run',        file: file_fixture_upload('dra/valid/example-0001_dra_Run.xml')
@@ -185,15 +226,23 @@ RSpec.describe DraValidator, type: :model do
     expect(validation.validation_reports).to include(
       {
         object_id: 'RunFile',
-        path:      'runfile1.xml',
         validity:  'valid',
-        details:   nil
+        details:   nil,
+
+        file: {
+          path: 'runfile1.xml',
+          url:  'http://www.example.com/api/validations/42/files/runfile1.xml'
+        }
       },
       {
         object_id: 'RunFile',
-        path:      'runfile2.xml',
         validity:  'valid',
-        details:   nil
+        details:   nil,
+
+        file: {
+          path: 'runfile2.xml',
+          url:  'http://www.example.com/api/validations/42/files/runfile2.xml'
+        }
       }
     )
   end
