ade388699d9a1c173600f14fca1bb1bb56b01149
Keita Urashima <ursm@ursm.jp>
2024-02-08 01:30:28 +0900

    Typing

--- web/types/global.d.ts
+++ web/types/global.d.ts
@@ -5,17 +5,20 @@ import { HelperLike } from '@glint/template';
 import type PageTitle from 'ember-page-title/template-registry';
 import type StyleModifier from 'ember-style-modifier/template-registry';
 import type TruthHelpers from 'ember-truth-helpers/template-registry';
+import type { Task } from 'ember-concurrency';
+
+interface PerformSignature {
+  Args: {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    Positional: [Task<any, any[]>];
+  };
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  Return: (...args: any[]) => any;
+}
 
 declare module '@glint/environment-ember-loose/registry' {
   export default interface Registry extends PageTitle, StyleModifier, TruthHelpers {
-    perform: HelperLike<{
-      Args: {
-        // eslint-disable-next-line @typescript-eslint/no-explicit-any
-        Positional: [any];
-      };
-
-      // eslint-disable-next-line @typescript-eslint/no-explicit-any
-      Return: (...args: any[]) => any;
-    }>;
+    perform: HelperLike<PerformSignature>;
   }
 }
