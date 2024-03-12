a96676332d3a64ce2e5328f62722a2cdbc5bde96
Keita Urashima <ursm@ursm.jp>
2024-03-13 00:26:39 +0900

    Remove unused method

--- noodles_gff-rb/ext/noodles_gff/src/lib.rs
+++ noodles_gff-rb/ext/noodles_gff/src/lib.rs
@@ -1,29 +1,9 @@
-use std::fs::File;
-use std::io::{BufRead, BufReader};
-use std::path::PathBuf;
-
-use magnus::{class, define_module, exception, function, Error, ExceptionClass, Module, RClass, RModule};
+use magnus::{class, define_module, function, Error, ExceptionClass, Module, RClass, RModule};
 use noodles_gff::{Directive, Line, Reader};
 
 fn parse(input: String) -> Result<(), Error> {
-    let reader = Reader::new(input.as_bytes());
+    let mut reader = Reader::new(input.as_bytes());
 
-    read_lines(reader)
-}
-
-fn parse_from_file(path: PathBuf) -> Result<(), Error> {
-    let reader = File::open(path)
-        .map(BufReader::new)
-        .map(Reader::new)
-        .map_err(|e| Error::new(exception::runtime_error(), e.to_string()))?;
-
-    read_lines(reader)
-}
-
-fn read_lines<T>(mut reader: Reader<T>) -> Result<(), Error>
-where
-    T: BufRead,
-{
     for (i, line) in reader.lines().enumerate() {
         match line {
             Ok(Line::Directive(Directive::StartOfFasta)) => {
@@ -51,7 +31,6 @@ fn init() -> Result<(), Error> {
     gff.define_class("Error", class::object().const_get::<_, RClass>("StandardError")?)?;
 
     gff.define_module_function("parse", function!(parse, 1))?;
-    gff.define_module_function("parse_from_file", function!(parse_from_file, 1))?;
 
     Ok(())
 }
