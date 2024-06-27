d1e0d10c9d37dc39541ffc438ef3d2e247b0285a
Keita Urashima <ursm@ursm.jp>
2024-06-27 20:48:26 +0900

    Improve DDBJ Validator validation messages

--- api/app/models/concerns/ddbj_validator.rb
+++ api/app/models/concerns/ddbj_validator.rb
@@ -1,41 +1,40 @@
 module DDBJValidator
   def validate(validation)
     validation.write_files_to_tmp do |dir|
-      validation.objs.without_base.each do |obj|
-        part = Faraday::Multipart::FilePart.new(dir.join(obj.path).to_s, 'application/octet-stream')
+      obj  = validation.objs.without_base.sole # either BioProject or BioSample
+      part = Faraday::Multipart::FilePart.new(dir.join(obj.path).to_s, 'application/octet-stream')
 
-        begin
-          res      = client.post('validation', obj._id.downcase => part)
-          ok, body = wait_for_finish(res.body.fetch(:uuid))
-        rescue Faraday::Error => e
+      begin
+        res      = client.post('validation', obj._id.downcase => part)
+        ok, body = wait_for_finish(res.body.fetch(:uuid))
+      rescue Faraday::Error => e
+        obj.validity_error!
+
+        obj.validation_details.create!(
+          severity: 'error',
+          message:  e.message
+        )
+
+        Rails.error.report e
+      else
+        validation.update! raw_result: body
+
+        if ok
+          obj.update! validity: body.dig(:result, :validity) ? 'valid' : 'invalid'
+
+          body.dig(:result, :messages).each do |error|
+            code, severity = error.fetch_values(:id, :level)
+            message        = message_for(error)
+
+            obj.validation_details.create! code:, severity:, message:
+          end
+        else
           obj.validity_error!
 
           obj.validation_details.create!(
             severity: 'error',
-            message:  e.message
+            message:  body.fetch(:message)
           )
-
-          Rails.error.report e
-        else
-          # each is executed only once, so it will not be overwritten by subsequent executions
-          validation.update! raw_result: body
-
-          if ok
-            obj.update! validity: body.dig(:result, :validity) ? 'valid' : 'invalid'
-
-            body.dig(:result, :messages).each do |msg|
-              code, severity, message = msg.fetch_values(:id, :level, :message)
-
-              obj.validation_details.create! code:, severity:, message:
-            end
-          else
-            obj.validity_error!
-
-            obj.validation_details.create!(
-              severity: 'error',
-              message:  body.fetch(:message)
-            )
-          end
         end
       end
     end
@@ -71,6 +70,40 @@ module DDBJValidator
       [false, result.body]
     else
       raise "must not happen: #{status.body.to_json}"
+    end
+  end
+
+  def message_for(error)
+    message     = error.fetch(:message)
+    annotations = error.fetch(:annotation, []).index_by { _1.fetch(:key) }
+
+    return message unless sample_name = annotations.dig('Sample name', :value)
+
+    case error.fetch(:id)
+    when 'BS_R0003'
+      %(The Sample title is not unique for the Sample name "#{sample_name}". Please provide a unique Sample title.)
+    when 'BS_R0013'
+      key       = annotations.fetch('Attribute').fetch(:value)
+      value     = annotations.fetch('Attribute value').fetch(:value)
+      suggested = annotations.fetch('Suggested value').fetch(:suggested_value).first
+
+      %(Invalid data format. The "#{key}" attribute value is not valid for the Sample name "#{sample_name}". Please replace "#{value}" to "#{suggested}".)
+    when 'BS_R0015'
+      host = annotations.fetch('host').fetch(:value)
+
+      %(Invalid host organism name. The "host" attribute value is not valid for the Sample name "#{sample_name}". Please correct "#{host}".)
+    when 'BS_R0045'
+      organism = annotations.fetch('organism').fetch(:value)
+
+      %(Warning about "organism" for the Sample name "#{sample_name}". Please correct "#{organism}". If applicable, the taxonomy id will be automatically filled and the organism will be corrected to the scientific name. When the organism(s) is novel, please enter proposed name(s) in the organism, leave the taxonomy id empty and submit the BioSample.)
+    when 'BS_R0100'
+      key       = annotations.fetch('Attribute name').fetch(:value)
+      value     = annotations.fetch('Attribute value').fetch(:value)
+      suggested = annotations.fetch('Suggested value').fetch(:suggested_value).first
+
+      %(Missing values are not neccesary for optional attributes. Leave values empty when there is no information. The "#{key}" attribute value is not valid for the Sample name "#{sample_name}". Please replace "#{value}" to "#{suggested}".)
+    else
+      message
     end
   end
 end
