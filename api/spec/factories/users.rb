2893efea20403eb3876ab8f439d71ec78ec36d40
Keita Urashima <ursm@ursm.jp>
2024-09-05 09:25:46 +0900

    Remove unused user attributes

--- api/spec/factories/users.rb
+++ api/spec/factories/users.rb
@@ -1,10 +1,5 @@
 FactoryBot.define do
   factory :user do
     sequence(:uid) { "user#{_1}" }
-
-    first_name   { 'Alice' }
-    last_name    { 'Liddell' }
-    organization { 'Wonderland Inc.' }
-    email        { 'test@example.com' }
   end
 end
