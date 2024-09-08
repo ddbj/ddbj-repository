2f716ecfae9a4a10fcfd313d3975ca0449f948bd
Keita Urashima <ursm@ursm.jp>
2024-09-08 22:32:55 +0900

    rjust

--- api/script/dump_bioproject_xml.rb
+++ api/script/dump_bioproject_xml.rb
@@ -3,5 +3,5 @@ require_relative '../config/environment'
 dir = Rails.root.join('tmp/bioproject_xml')
 
 BioProject::XML.find_each do |xml|
-  dir.join("#{xml.submission_id}-#{xml.version}.xml").write xml.content
+  dir.join("#{xml.submission_id}-#{xml.version.to_s.rjust(3, '0')}.xml").write xml.content
 end
