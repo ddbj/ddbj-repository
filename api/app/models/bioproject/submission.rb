40412008652d66702464ceae6f77265d10bdd08e
Keita Urashima <ursm@ursm.jp>
2024-09-10 09:04:19 +0900

    Cosmetic

--- api/app/models/bioproject/submission.rb
+++ api/app/models/bioproject/submission.rb
@@ -3,9 +3,9 @@ class BioProject::Submission < BioProject::Record
 
   has_one :project, class_name: "BioProject::Project"
 
-  has_many :action_histories,  class_name: "BioProject::ActionHistory"
-  has_many :submission_data, class_name: "BioProject::SubmissionDatum"
-  has_many :xmls,            class_name: "BioProject::XML"
+  has_many :action_histories, class_name: "BioProject::ActionHistory"
+  has_many :submission_data,  class_name: "BioProject::SubmissionDatum"
+  has_many :xmls,             class_name: "BioProject::XML"
 
   enum :status_id, {
     data_submitted: 700
