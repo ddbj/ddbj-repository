5c932e177508f5618694c730b5364a3802f7ec03
Keita Urashima <ursm@ursm.jp>
2024-09-10 16:10:44 +0900

    Dump latest XML

--- api/script/bioproject_dump.rb
+++ api/script/bioproject_dump.rb
@@ -2,6 +2,14 @@ require_relative '../config/environment'
 
 dir = Rails.root.join('tmp/bioproject_xml')
 
-BioProject::XML.find_each do |xml|
-  dir.join("#{xml.submission_id}-#{xml.version.to_s.rjust(3, '0')}.xml").write xml.content
+cond = <<~SQL
+  (submission_id, version) IN (
+    SELECT submission_id, MAX(version)
+    FROM xml
+    GROUP BY submission_id
+  )
+SQL
+
+BioProject::XML.where(cond).find_each do |xml|
+  dir.join("#{xml.submission_id}.xml").write xml.content
 end
