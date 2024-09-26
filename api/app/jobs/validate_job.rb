5a1bb5827c859e09b3a0c59199c85c61331f8511
Keita Urashima <ursm@ursm.jp>
2024-09-26 16:24:13 +0900

    Refactor

--- api/app/jobs/validate_job.rb
+++ api/app/jobs/validate_job.rb
@@ -21,17 +21,15 @@ class ValidateJob < ApplicationJob
   def perform(validation)
     validation.update! progress: :running, started_at: Time.current
 
-    begin
-      ActiveRecord::Base.transaction do
-        validator = "Database::#{validation.db}::Validator".constantize.new
+    ActiveRecord::Base.transaction do
+      validator = "Database::#{validation.db}::Validator".constantize.new
 
-        validator.validate validation
-        validation.objs.base.validity_valid! unless validation.objs.base.validity
+      validator.validate validation
+      validation.objs.base.validity_valid! unless validation.objs.base.validity
 
-        raise ActiveRecord::Rollback if validation.reload.canceled?
-      end
-    ensure
-      validation.update! progress: :finished, finished_at: Time.current unless validation.canceled?
+      raise ActiveRecord::Rollback if validation.reload.canceled?
     end
+  ensure
+    validation.update! progress: :finished, finished_at: Time.current unless validation.canceled?
   end
 end
