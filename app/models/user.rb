8d4af67ac6e4e914e97dbfa054f4c344a1153ba5
Keita Urashima <ursm@ursm.jp>
2025-04-02 13:51:21 +0900

    Use base58

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   def self.generate_api_key
-    "repository_#{Base62.encode(SecureRandom.random_number(2 ** 256))}"
+    SecureRandom.base58(32)
   end
 
   has_many :validations
