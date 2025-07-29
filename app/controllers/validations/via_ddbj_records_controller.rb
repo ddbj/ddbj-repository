b8ad46b0465c5abb8c23f6acc1ceca16f4dc5ff5
maimux2x <m0smile10often@gmail.com>
2025-07-29 11:21:28 +0900

    Keep errors that are not linked to files

--- app/controllers/validations/via_ddbj_records_controller.rb
+++ app/controllers/validations/via_ddbj_records_controller.rb
@@ -1,7 +1,9 @@
 class Validations::ViaDDBJRecordsController < ApplicationController
   def create
-    @validation = current_user.validations.create!(db: params[:db], progress: :finished, finished_at: Time.current)
-    @validation.objs.create! file: params["DDBJ Record"]["file"], _id: "DDBJ Record", validity: "valid"
+    @validation = current_user.validations.create!(db: params[:db], progress: :finished, finished_at: Time.current) { |validation|
+      validation.objs.build _id: "_base",       validity: "valid"
+      validation.objs.build _id: "DDBJ Record", validity: "valid", file: params["DDBJ Record"]["file"]
+    }
 
     render status: :created
   end
