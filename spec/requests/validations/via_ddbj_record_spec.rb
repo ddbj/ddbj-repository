bbd2878ff8211ee6b7cde27efe36e450b1b2702a
Keita Urashima <ursm@ursm.jp>
2025-07-30 11:24:25 +0900

    Cop

--- spec/requests/validations/via_ddbj_record_spec.rb
+++ spec/requests/validations/via_ddbj_record_spec.rb
@@ -1,18 +1,18 @@
-require "rails_helper"
+require 'rails_helper'
 
-RSpec.describe "validate via DDBJRecord", type: :request, authorized: true do
-  let_it_be(:user) { create_default(:user, uid: "alice") }
+RSpec.describe 'validate via DDBJRecord', type: :request, authorized: true do
+  let_it_be(:user) { create_default(:user, uid: 'alice') }
 
   before do
     default_headers[:Authorization] = "Bearer #{user.token}"
   end
 
-  example "happy case" do
-    post "/api/validations/via_ddbj_record", params: {
-      db: "Trad",
+  example 'happy case' do
+    post '/api/validations/via_ddbj_record', params: {
+      db: 'Trad',
 
       DDBJRecord: {
-        file: uploaded_file(name: "JP2022130675.json")
+        file: uploaded_file(name: 'JP2022130675.json')
       }
     }
 
