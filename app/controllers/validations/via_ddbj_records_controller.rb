a3517378c34a5f9f7cd362d791990b15c5cfa9e7
Keita Urashima <ursm@ursm.jp>
2025-07-30 11:24:25 +0900

    Cop

--- app/controllers/validations/via_ddbj_records_controller.rb
+++ app/controllers/validations/via_ddbj_records_controller.rb
@@ -1,8 +1,8 @@
 class Validations::ViaDDBJRecordsController < ApplicationController
   def create
-    @validation = current_user.validations.create!(db: params[:db], progress: :finished, finished_at: Time.current) { |validation|
-      validation.objs.build _id: "_base",      validity: "valid"
-      validation.objs.build _id: "DDBJRecord", validity: "valid", file: params["DDBJRecord"]["file"]
+    @validation = current_user.validations.create!(db: params[:db], progress: :finished, finished_at: Time.current) {|validation|
+      validation.objs.build _id: '_base',      validity: 'valid'
+      validation.objs.build _id: 'DDBJRecord', validity: 'valid', file: params['DDBJRecord']['file']
     }
 
     render status: :created
