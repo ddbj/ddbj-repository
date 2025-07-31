28e78552c8d6e091c807268881c4941034fb0f96
Keita Urashima <ursm@ursm.jp>
2025-07-31 11:57:40 +0900

    WIP: Write back to DDBJ Record

--- app/models/database/trad/submitter.rb
+++ app/models/database/trad/submitter.rb
@@ -11,12 +11,28 @@ class Database::Trad::Submitter
     ActiveRecord::Base.transaction do
       start_num = Sequence.claim('accessions.number/AB', count: entries.size)
 
-      submission.accessions.insert_all entries.map.with_index {|entry, i|
+      entry_id_to_attrs = submission.accessions.insert_all(entries.map.with_index {|entry, i|
         {
           number:   "AB#{(start_num + i).to_s.rjust(6, '0')}",
           entry_id: entry[:id]
         }
-      }
+      }, **{
+        unique_by: %i[number entry_id version],
+        returning: %i[entry_id number version last_updated_at]
+      }).index_by { it['entry_id'] }.transform_values(&:deep_symbolize_keys)
+
+      entries.each do |entry|
+        entry_id_to_attrs.fetch(entry[:id]) => {number: accession, version:, last_updated_at:}
+
+        entry.update(
+          accession:,
+          locus:        accession,
+          version:,
+          last_updated: Time.zone.parse(last_updated_at).iso8601
+        )
+      end
     end
+
+    puts JSON.pretty_generate(record)
   end
 end
