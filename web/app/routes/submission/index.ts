b7895245e3a865d821522707ff67093e070aa59d
maimux2x <m0smile10often@gmail.com>
2025-10-14 10:43:17 +0900

    Refresh

--- web/app/routes/submission/index.ts
+++ web/app/routes/submission/index.ts
@@ -1,16 +1,20 @@
 import Route from '@ember/routing/route';
+import { service } from '@ember/service';
 
+import type RouterService from '@ember/routing/router-service';
 import type { components } from 'schema/openapi';
 
 type Submission = components['schemas']['Submission'];
 
 export default class SubmissionIndexRoute extends Route {
+  @service declare router: RouterService;
+
   timer?: number;
 
   afterModel({ progress }: Submission) {
     if (progress === 'waiting' || progress === 'running') {
       this.timer = setTimeout(() => {
-        this.refresh();
+        this.router.refresh();
       }, 2000);
     }
   }
