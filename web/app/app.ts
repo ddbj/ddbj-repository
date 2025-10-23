5ac1ef43e9d33b0e3871bf420a491a961f776325
Keita Urashima <ursm@ursm.jp>
2025-10-23 16:10:31 +0900

    Update Ember

--- web/app/app.ts
+++ web/app/app.ts
@@ -1,7 +1,7 @@
 import Application from '@ember/application';
 import Resolver from 'ember-resolver';
 import loadInitializers from 'ember-load-initializers';
-import config from 'repository/config/environment';
+import config from './config/environment';
 import { importSync, isDevelopingApp, macroCondition } from '@embroider/macros';
 
 if (macroCondition(isDevelopingApp())) {
@@ -11,10 +11,12 @@ if (macroCondition(isDevelopingApp())) {
 export default class App extends Application {
   modulePrefix = config.modulePrefix;
   podModulePrefix = config.podModulePrefix;
-  Resolver = Resolver;
+  Resolver = Resolver.withModules(compatModules);
 }
 
-loadInitializers(App, config.modulePrefix);
+import compatModules from '@embroider/virtual/compat-modules';
+
+loadInitializers(App, config.modulePrefix, compatModules);
 
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.css';
