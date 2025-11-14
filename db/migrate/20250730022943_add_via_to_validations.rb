2c06cdbad932121668da6aa3b75856410bbf7009
Keita Urashima <ursm@ursm.jp>
2025-11-14 14:08:01 +0900

    Fix migration

--- db/migrate/20250730022943_add_via_to_validations.rb
+++ db/migrate/20250730022943_add_via_to_validations.rb
@@ -1,5 +1,11 @@
 class AddViaToValidations < ActiveRecord::Migration[8.0]
   def change
-    add_column :validations, :via, :string, null: false
+    add_column :validations, :via, :string
+
+    execute <<~SQL
+      UPDATE validations SET via = 'file';
+    SQL
+
+    change_column_null :validations, :via, false
   end
 end
