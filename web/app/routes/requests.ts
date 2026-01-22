4422e291eaabd0065c17620719d30c3b943a890e
Keita Urashima <ursm@ursm.jp>
2026-01-22 16:26:04 +0900

    Remove unused

--- web/app/routes/requests.ts
+++ web/app/routes/requests.ts
@@ -2,12 +2,10 @@ import Route from '@ember/routing/route';
 import { service } from '@ember/service';
 
 import type CurrentUserService from 'repository/services/current-user';
-import type RequestService from 'repository/services/request';
 import type Transition from '@ember/routing/transition';
 
 export default class SubmissionsRoute extends Route {
   @service declare currentUser: CurrentUserService;
-  @service declare request: RequestService;
 
   beforeModel(transition: Transition) {
     this.currentUser.ensureLogin(transition);
